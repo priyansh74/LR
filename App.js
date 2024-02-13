4f70aed5ec118824ff2b140755c4322e6ecbddfd
priyansh74 <priyanshjha99@gmail.com>
2024-02-13 23:51:00 +0530

    L-1

---
+++ App.js
@@ -0,0 +1,27 @@
+/*
+  div d=parent
+    div=child1
+      h1 
+      h2
+    div= child2
+     h1
+     h2
+  div
+*/
+
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ]),React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ])]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
