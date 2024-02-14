8c5f9a1a3a54c2d415a22108e3fb8b17050a580d
priyansh74 <priyanshjha99@gmail.com>
2024-02-14 22:20:25 +0530

    2.0

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-/*
-  div d=parent
-    div=child1
-      h1 
-      h2
-    div= child2
-     h1
-     h2
-  div
-*/
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const parent = React.createElement(
   "div",
   { id: "parent" },
   [React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h1", {}, "This is Namaste React"),
     React.createElement("h2", {}, "I am an h2 tag"),
   ]),React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I am an h1 tag"),
